b5cbd6b50e5feac4fad423e6f11d3b2c26f5557b
Manav Sehgal <sehgal.manav@gmail.com>
2016-04-13 10:34:03 +0530

    Init

--- code/app/components/hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Hello({ greet, message }) {
-  return (
-    <div className="title-shadow">
-      {greet} {message}
-    </div>
-  );
-}
--- code/app/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import World from './components/World.jsx';
-
-ReactDOM.render(
-  <World greet="Hello" />,
-  document.getElementById('app')
-);
