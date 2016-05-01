16c91ee9ef32a7ae535b418e22b4c61c26391ab5
Manav Sehgal <sehgal.manav@gmail.com>
2016-05-01 08:13:24 +0530

    More Sample chapters

--- code/app/components/LeanPub.jsx
+++ code/app/components/LeanPub.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LeanPub(props) {
   const source = `https://leanpub.com/${props.bookid}/embed`;
   return (
-    <iframe width="100%" height="360"
+    <iframe width="100%" height="370"
       src={source}
       frameborder="0" allowtransparency="true">
     </iframe>
