2ee3669c9ac0f6ae01905579e01dc9760963b472
Manav Sehgal <sehgal.manav@gmail.com>
2016-05-31 15:10:13 +0530

    Redux Dev Tools

--- code/app/store/roadmap.js
+++ code/app/store/roadmap.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
 import roadmapApp from '../reducers/roadmapApp';
-const store = createStore(roadmapApp);
+const store = createStore(roadmapApp, window.devToolsExtension && window.devToolsExtension());
 export default store;
